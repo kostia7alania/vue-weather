356201288f7208b2c3f214774985558daeac5aa1
kostia7alania <osetia-alania@mail.ru>
2019-02-27 00:17:01 +0300

    cleaning

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,4 @@
 
 export default  {
-  
-  getterDictionaries: s=>s.dictionaries, 
+   
 } 
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,31 +1,4 @@
-export default {
-  appErrorsCommit(obj, {index, prop,state}){// console.log('appErrorsCommit', arguments)
-    obj[prop][index] = state;
-    obj.globalErrorStatus = obj.appErrors.some(e=>e=='error');
-  },
-
-  save_mutator(obj, {prop, state}){ },//example;  
-  /* maximize: s => { s.global_state = 1 }*/
-  
-  setRand(obj, {prop,state}) {
-    obj[prop] = state > 0 ? +new Date() : -new Date();
-  },
-  
-  sett(obj, {prop,state}) { obj[prop] = obj[prop] == state ? "": state; },//2 раза незя клик
-  /* 
-    search_mutator(obj, {prop, state}) { obj[prop] = obj[prop] == state ? "": state; }//2 раза незя клик
-    obj[prop].leadNumber = obj[prop].leadNumber == state.leadNumber ? "": state.leadNumber; }//второй клик по той же ячейке - уьирает выделение
-  },
-  */
-
-  state_mutator(s, {prop, val}) {
-    s[prop] = val;
-  },
-
-  obj_mutator (s, {obj, prop, val}) {
-    //console.error('obj_mutator',arguments)
-    s[obj][prop] = val;
-  },
+export default { 
 
   changeProp (store, {prop, state}) {
     console.log(arguments)
