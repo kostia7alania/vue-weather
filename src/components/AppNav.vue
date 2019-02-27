d2de9ff3ea461f048860a78941ad23a1d882c08b
kostia7alania <osetia-alania@mail.ru>
2019-02-27 21:42:55 +0300

    work with NAVbar // TODO: loading.

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,29 +1,56 @@
 <template>
   <nav>
-    <button id="locateBtn" :class="{pulse: positionEnabled}" @click="getPosition">
-     <i class="fa fa-location-arrow" aria-hidden="true"></i>
+    <button class="locateBtn" :class="{pulse: positionEnabled}" @click="getPosition" v-tooltip="positionEnabled?'Update my weather':'Get my weather'">
+      <i class="fa fa-location-arrow" aria-hidden="true"></i>
     </button>
-    <button id="unitBtn" @click="celsiusToggle">{{celsius?'c':'f'}}</button>
-   
     
+    <button v-if="!loading" id="unitBtn" @click="celsiusToggle" v-tooltip="`To ${!celsius?'celsius':'fahrenheit'}`">{{celsius?'c':'f'}}</button>
+    <button v-else id="unitBtn" v-tooltip="'loading'">
+      <i class="fa fa-spin fa-pulse" aria-hidden="true"/>
+    </button>
+    
+    <button class="locateBtn" @click="routeToggle" :class="{on: favoritesOpen}" v-tooltip="`${favoritesOpen?'Hide':'Show'} favorites`">
+      <i class="fa fa-star" aria-hidden="true"/>
+    </button>
+		
   </nav>
 </template>
 
 <script>
 export default {
-  name: "AppNav",
+	name: "AppNav",
+	data(){
+		return {
+			favoritesOpen: null, 
+		}
+  },
+  mounted() {
+  
+  },
   methods: {
+		
+    routeToggle () {
+			if(this.$router.history.current.name == 'Favorites') {
+				this.favoritesOpen = false;
+				this.$router.push({name: 'Home'});
+			} else {
+				this.favoritesOpen = true;
+				this.$router.push({name: 'Favorites'});
+			}  
+		},
+		
     getPosition() {
-      this.$emit('get-position')
+      this.$store.dispatch("getPosition",{that:this, toast: this.$toast});
     },
-    celsiusToggle() { 
-      this.$store.commit('toggle',{prop: 'celsius'})
+
+    celsiusToggle() {
+      this.$store.commit("toggle", { prop: "celsius" });
     }
   },
-  
+
   computed: {
-    positionEnabled(){
-      return this.$store.state.positionEnabled
+    loading() {
+      return this.$store.state.loading;
     },
     celsius() {
       return this.$store.state.celsius;
@@ -32,56 +59,60 @@ export default {
 };
 </script>
 
-<style scoped>  
-
-
+<style scoped>
 /*----------------
 	Buttons
 -----------------*/
 button {
-	border: none;
-	cursor: pointer;
+  border: none;
+  cursor: pointer;
+  margin: 4px;
 }
 nav {
-	font-size: 1rem;
-	margin: 0 auto;
-	padding: 5% 0 10%;
+  font-size: 1rem;
+  margin: 0 auto;
+  padding: 5% 0 10%;
 }
-#locateBtn, #unitBtn {
-	background: none;
-	border: 1px solid rgba(255,255,255,0.25);
-	border-radius: 50%;
-	color: #fff;
-	height: 40px;
-	transition: background 1s ease-in-out, border 0.2s ease-in-out;
-	width: 40px;
+.locateBtn,
+#unitBtn {
+  background: none;
+  border: 1px solid rgba(255, 255, 255, 0.25);
+  border-radius: 50%;
+  color: #fff;
+  height: 40px;
+  transition: background 1s ease-in-out, border 0.2s ease-in-out;
+  width: 40px;
 }
-#locateBtn.on, #unitBtn.on {
-	background: rgba(255,255,255,0.25);
+.locateBtn.on,
+#unitBtn.on {
+  background: rgba(255, 255, 255, 0.25);
 }
-#locateBtn:focus, #locateBtn:hover, #unitBtn:focus, #unitBtn:hover {
-	border: 1px solid rgba(255,255,255,0.75);
+.locateBtn:focus,
+.locateBtn:hover,
+#unitBtn:focus,
+#unitBtn:hover {
+  border: 1px solid rgba(255, 255, 255, 0.75);
 }
-#locateBtn {
-	margin-right: 10px;
+.locateBtn {
+  /*margin-right: 10px;*/
 }
 #unitBtn {
-	font-weight: 300;
-	padding-right: 3px;
-	padding-top: 2px;
-	text-transform: uppercase;
+  font-weight: 300;
+  padding-right: 3px;
+  padding-top: 2px;
+  text-transform: uppercase;
 }
 #unitBtn:before {
-	content: '\00b0'; /* Degree symbol */
-	padding: 1px;
+  content: "\00b0"; /* Degree symbol */
+  padding: 1px;
 }
 /* locateBtn Pulse Animation */
-#locateBtn.pulse {
-	animation: pulse 2s infinite;
+.locateBtn.pulse {
+  animation: pulse 2s infinite;
 }
 @keyframes pulse {
-	20% { background: rgba(255,255,255,0.25); }
+  20% {
+    background: rgba(255, 255, 255, 0.25);
+  }
 }
-
-
 </style>
