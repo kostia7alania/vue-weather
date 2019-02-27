c45110cb308534182e9694a279e5664cb7dbab37
kostia7alania <osetia-alania@mail.ru>
2019-02-27 21:40:30 +0300

    work with store

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,27 @@
 const state = {
   tokenOpenWeather: "a3e7bdc246b811691b06aab13ccb0dbb",
+  tokenGoogleMap: "AIzaSyAAFOmrJq6MZXgYg42dfA-vsu9nVD8amhQ",
+  tokenIpstack:"5fd8d637b290b3d7cecf4c81cfbf7fcc",
   celsius: true,
-  positionEnabled: false
+  positionEnabled: false,
+  favorites: {},
+  getTime: () => `${(new Date).getHours()}:${(new Date).getMinutes()}:${(new Date).getSeconds()}`,
+  measurements: {
+    cloudiness: 0,
+    windSpeed: 0,
+    humidity: 0
+  },
+  temperature: {
+    value: 0,
+    high: 0,
+    low: 0
+  },
+  weather: {
+    location: null, 
+    description: 'Connection isn\'t available!',
+    icon: require("../assets/icons/weather/cloud.svg")
+  },
+  loading: false
 }
 
 import Vue from 'vue'
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,16 +3,33 @@ export default {
   changeProp (store, {prop, state}) {
     console.log(arguments)
     store[prop] = state;
+    Vue.setVuexStorage();
   },
 
   changeObj (store, {obj, prop, state}) {
     console.log(arguments)
     store[obj][prop] = state;
+    Vue.setVuexStorage();
   },
 
   toggle (store, {prop}) { 
     store[prop] = !store[prop];
+    Vue.setVuexStorage();
+  },  
+  
+  appendToFavorite (store, {prop, state}) {
+    Vue.$set(store[prop], state.id, state);
+    Vue.setVuexStorage();
+  },
+  setObj (store, {prop, state}) {
+    Vue.$set(store, prop, state);
+    Vue.setVuexStorage();
   },
 
+  deleteCity (store, id) {
+    console.log(arguments, store.favorites[id])
+    //delete store.favorites[id]
+    Vue.$delete(store.favorites, id);
+  },
 }
  
