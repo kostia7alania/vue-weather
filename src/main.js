0b1d8fcdbeb88f48f724954850972daf461b8e97
kostia7alania <osetia-alania@mail.ru>
2019-02-27 21:39:44 +0300

    add to vue router, tooltips, alerts, global mixins,

--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store/'
-import './registerServiceWorker' 
+import './registerServiceWorker'
+import router from "./router";
+
+import VueIziToast from "vue-izitoast";
+import 'izitoast/dist/css/iziToast.min.css';
+Vue.use(VueIziToast);
+
+import VTooltip from 'v-tooltip'
+Vue.use(VTooltip)
+
+import { globalMixin }  from './mixins/';
+Vue.mixin(globalMixin);
 
 Vue.config.productionTip = false
 Vue.config.devtools = true;
- 
-import router from "./router";
 
-import globalMixin from './mixins/';
-Vue.mixin(globalMixin);
-
-new Vue({
+window.Vue = new Vue({
   router,
   store,
   render: h => h(App)
