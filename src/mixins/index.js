7314a3ae144e6bdc44b243abb0e87e4dcf94a331
kostia7alania <osetia-alania@mail.ru>
2019-02-27 21:39:55 +0300

    add mixins

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,10 +1,90 @@
-const globalMixin = {
-    computed: {
-        period: () => {
-            let hour = (new Date).getHours();
-            return (hour > 5 && hour < 18) ? 'app--day' : 'app--night';
+export const localDataMixin = {
+    methods: {
+        initLocalStorage(name) {
+            if (localStorage) {
+                if (localStorage.getItem(name)) {
+                    let local = JSON.parse(localStorage.getItem(name));
+                    Object.keys(local).forEach(key => (this[key] = local[key]));
+                }
+            } else {
+                console.log("localStorage isn't support!");
+            }
+        },
+
+        setLocalStorage(name) {
+            window.t = this;
+            if (localStorage) {
+                localStorage.setItem(name, JSON.stringify(this._data));
+            } else {
+                console.log("localStorage isn't support!");
+            }
         }
     }
 };
 
-export default globalMixin;
+
+
+
+export const VuexMixin = {
+    methods: {
+        initVuexStorage(name) {
+            if (localStorage) {
+                console.log('READ')
+                let name = 'VuexStore';
+                if (localStorage.getItem(name)) {
+                    let local = JSON.parse(localStorage.getItem(name));
+                    Object.keys(local).forEach(key => {
+                        console.log(this.$store.state, key, local[key])
+                        this.$set(this.$store.state, key, local[key])
+
+                    });
+                }
+            } else {
+                console.warn("localStorage isn't support!");
+            }
+        },
+
+        setVuexStorage() {
+            if (localStorage) {
+                localStorage.setItem('VuexStore', JSON.stringify({
+                    ...this.$store.state,
+                    favorites: {
+                        ...this.$store.state.favorites
+                    }
+                }));
+            } else {
+                console.warn("localStorage isn't support!");
+            }
+        }
+    }
+};
+
+
+
+
+export const globalMixin = {
+    mixins: [localDataMixin, VuexMixin],
+    data() {
+        return {
+            alertOptions: {
+                position: "bottomRight"
+            }
+        }
+    },
+    methods: {
+        getTime() {
+            return `${(new Date).getHours()}:${(new Date).getMinutes()}:${(new Date).getSeconds()}`;
+        }
+    },
+    computed: {
+        period: () => {
+            let hour = (new Date).getHours();
+            return (hour > 5 && hour < 18) ? 'app--day' : 'app--night';
+        },
+        positionEnabled() {
+            return this.$store.state.positionEnabled;
+        },
+
+
+    }
+};
