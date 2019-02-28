ec0a9c6313a8c43db53f0485a66ed6d53445593a
kostia7alania <osetia-alania@mail.ru>
2019-02-28 10:16:43 +0300

    check updates when last DESCRIPTION state is CONNECT ISNT AVAILABLE

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,41 @@
 <template>
-  <div id="app">
-    <div class="app"  :class="period">
-      <WeatherApp :class="period"/>
-      <router-view :class="period"/>
-      <AppCredits :class="period"/>
-    </div>
+  <div :class="period" class="app">
+    <WeatherApp/>
+    <router-view/>
+    <AppCredits/>
   </div>
 </template>
 
 
 <script>
-
-import WeatherApp from './components/WeatherApp'
-import AppCredits from './components/AppCredits'
+import WeatherApp from "./components/WeatherApp";
+import AppCredits from "./components/AppCredits";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     WeatherApp,
     AppCredits
-  }, 
-  data() {
-    return { }
   },
-  mounted() {
+  data() {
+    return {};
+  },
+  methods: {
+  },
+  mounted() { // write to Store from localStorage
     this.initVuexStorage();
+    if(this.description == 'Connection isn\'t available!') {
+      this.$store.dispatch("getPosition");
+    }
+  },
+  computed: {
+    description() {
+      return this.$store.state.weather.description
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
-@import './assets/global-styles.scss';
+@import "./assets/global-styles.scss";
 </style>
