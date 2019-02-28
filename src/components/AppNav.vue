b9a916e343a941389154521a2848adcb00981416
kostia7alania <osetia-alania@mail.ru>
2019-02-28 10:17:27 +0300

    add status displaying

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,54 +1,77 @@
 <template>
   <nav>
-    <button class="locateBtn" :class="{pulse: positionEnabled}" @click="getPosition" v-tooltip="positionEnabled?'Update my weather':'Get my weather'">
+    <button
+      class="locateBtn"
+      :class="{pulse: positionEnabled}"
+      @click="getPosition"
+      v-tooltip="positionEnabled?'Update my weather':'Get my weather'"
+    >
       <i class="fa fa-location-arrow" aria-hidden="true"></i>
     </button>
     
-    <button v-if="!loading" id="unitBtn" @click="celsiusToggle" v-tooltip="`To ${!celsius?'celsius':'fahrenheit'}`">{{celsius?'c':'f'}}</button>
-    <button v-else id="unitBtn" v-tooltip="'loading'">
-      <i class="fa fa-spin fa-pulse" aria-hidden="true"/>
-    </button>
+    <button
+      id="unitBtn"
+      @click="celsiusToggle"
+      v-tooltip="`To ${!celsius?'celsius':'fahrenheit'}`"
+    >{{celsius?'c':'f'}}</button>
     
-    <button class="locateBtn" @click="routeToggle" :class="{on: favoritesOpen}" v-tooltip="`${favoritesOpen?'Hide':'Show'} favorites`">
+    <button
+      class="locateBtn"
+      @click="routeToggle"
+      :class="{on: favoritesOpen}"
+      v-tooltip="`${favoritesOpen?'Hide':'Show'} favorites`"
+    >
       <i class="fa fa-star" aria-hidden="true"/>
     </button>
-		
+    
+    <button class="locateBtn" v-tooltip="status.tooltip" @click="statusBtnClick">
+      <i :class="status.class" aria-hidden="true"/>
+    </button>
   </nav>
 </template>
-
+ 
 <script>
 export default {
-	name: "AppNav",
-	data(){
-		return {
-			favoritesOpen: null, 
-		}
-  },
-  mounted() {
-  
+  name: "AppNav",
+  data() {
+    return {
+      favoritesOpen: null,
+      online: navigator.onLine,
+    };
   },
   methods: {
-		
-    routeToggle () {
-			if(this.$router.history.current.name == 'Favorites') {
-				this.favoritesOpen = false;
-				this.$router.push({name: 'Home'});
-			} else {
-				this.favoritesOpen = true;
-				this.$router.push({name: 'Favorites'});
-			}  
-		},
-		
-    getPosition() {
-      this.$store.dispatch("getPosition",{that:this, toast: this.$toast});
+    statusBtnClick() {
+      this.online = navigator.onLine;
+      let loading = { method: "info", text: " Loading..Please wait..." };
+      let offline = { method: "warning", text: "You offline. Please check connection!" };
+      let okay = { method: "success", text: "Status: All fine!" };
+      let out = this.loading ? loading : !this.online ? offline : okay;
+      this.$toast[out.method](out.text, this.getTime(),this.alertOptions);
     },
-
+    routeToggle() {
+      if (this.$router.history.current.name == "Favorites") {
+        this.favoritesOpen = false;
+        this.$router.push({ name: "Home" });
+      } else {
+        this.favoritesOpen = true;
+        this.$router.push({ name: "Favorites" });
+      }
+    },
+    getPosition() {
+      this.$store.dispatch("getPosition");
+    },
     celsiusToggle() {
       this.$store.commit("toggle", { prop: "celsius" });
     }
   },
 
   computed: {
+    status() {
+      let loading = { tooltip: "Loading", class: "fas fa-spinner fa-pulse" };
+      let offline = { tooltip: "Offline", class: "fa fa-ban" };
+      let okay = { tooltip: "OK", class: "fa fa-check" };
+      return this.loading ? loading : !this.online ? offline : okay;
+    },
     loading() {
       return this.$store.state.loading;
     },
